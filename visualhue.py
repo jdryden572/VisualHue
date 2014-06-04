3539dc6a1be2296814ce15fa0a3bc61540cd313f
jdryden572 <jdryden572@gmail.com>
2014-06-04 17:00:10 -0400

    initial clean up

--- visualhue.py
+++ visualhue.py
@@ -1,17 +1,6 @@
 import requests
 import json
 
-### Attempt to find IP using nupnp site ###
-# note: this function may not work if we have more than one Bridge installed
-def getBridgeIP():
-  data = requests.get('http://www.meethue.com/api/nupnp')
-  if data.text != '[]':
-    return json.loads(data.text).get('internalipaddress')
-  else:
-    print('Could not locate Bridge automatically.')
-    return None
-
-
 
 class Hue:
   def __init__(self, IP, userName, deviceType)
