e7fabdf819d69e198d28170317dda9c344e1169a
jdryden572 <jdryden572@gmail.com>
2014-06-05 09:44:30 -0400

    Imported phue

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-class Request:
-  def request(self, method = 'GET', url = '', data = {}):
-    url = 'http://{0}/api/{1}{2}'.format(self.IP, self.userName, url)
-    response = requests.request(method, url, data)
-    toReturn = json.loads(response.content)
